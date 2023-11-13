12c7921804b53480b26dbe1117f861a542f464b1
bilal <muhmdbilal3333@gmail.com>
2023-11-13 15:17:13 +0500

    list of property add

--- server/routes/v2/questionnaire.js
+++ server/routes/v2/questionnaire.js
@@ -2,27 +2,34 @@ const express = require("express");
 const app = express();
 const {checkAminAuthMiddleware} = require("../../middlewares")
 const Questionnaire = require("../../models/questionnaire");
+const ListOfPropertiesSchema = require("../../models/listofproperties");
 
-// get questionnaire by user Id
+// get questionnaire by user Id  
 app.get('/getQuestionnaireByUserId', checkAminAuthMiddleware, async (req, res) => {
-    try {
-        const User_id = req.query.user_id;
-        const questionnaire = await Questionnaire.find({userId: User_id});
-        if (!questionnaire) {
-            return res
-                .status(404)
-                .json({ success: false, message: "Questionnaire record not found." });
-        }
+  try {
+      const userId = req.query.user_id;
+      const questionnaires = await Questionnaire.find({ userId });
+      if (!questionnaires || questionnaires.length === 0) {
+          return res.status(404).json({ success: false, message: "Questionnaire records not found." });
+      }
+      let result = [];
+      for (const questionnaire of questionnaires) {
+          const properties = await ListOfPropertiesSchema.find({ questionnaireId: questionnaire._id.toString() }).select("_id title price address destination link image");
+          const questionnaireData = {
+              questionnaire,
+              properties
+          };
+          result.push(questionnaireData);
+      }
       res.status(200).json({
           success: true,
-          data: questionnaire,
+          data: result,
           message: 'Get All Questionnaires.',
           code: 'getAllQuestionnaire_API',
-        });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
-  });
+      });
+  } catch (err) {
+      res.status(500).json({ success: false, error: err });
+  }
+});
 
   module.exports = app;
